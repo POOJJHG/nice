94aec0b8193c33a7799d8762d2a2de6df042265b
POOJJHG <sadb67871@gmail.com>
2022-11-08 22:07:23 +0700

    nice

---
+++ plugins/info-donasi.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+「 Suport Bot Dengan Cara donasi 」
+
+⫹⫺ Via Pulsa XL : 6281807254833
+⫹⫺ Via Dana : 6281807254833
+⫹⫺ Via Saweria :
+
+
+`.trim()
+  m.reply(text)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
